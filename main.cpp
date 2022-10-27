5c4323a055f2b0a91062564cd7d13e791bdff5ed
Erik Pihl <100573213+ErikPihl@users.noreply.github.com>
2022-10-27 18:55:43 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/********************************************************************************
+* main.cpp: Demonstration av klass för enkel styrning av lysdioder.
+********************************************************************************/
+#include "led.hpp"
+
+/********************************************************************************
+* main: Ansluter en lysdiod till pin 8 / PORTB0. Denna lysdiod sätts till att
+*       blinka kontinuerligt var 100:e millisekund.
+********************************************************************************/
+int main(void)
+{
+   led l1(8);
+
+   while (1)
+   {
+      l1.blink(100);
+   }
+
+   return 0;
+}
+
